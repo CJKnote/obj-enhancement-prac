de30cec2097d35b56642f97bcec01d6b639e3a93
Caroline Knote <cj.knote@gmail.com>
2021-12-22 22:16:17 -0800

    implementing methods

---
+++ objEnhancements.js
@@ -0,0 +1,61 @@
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+
+/* Write an ES2015 Version */
+const createInstructor =(firstName, lastName) => ({firstName, lastName});
+
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+
+/* Write an ES2015 Version */
+let favoriteNumber = 42;
+let instructor = {
+    firstName: "Colt"
+    [favoriteNumber] = "That is my favorite!"
+}
+
+
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi: function(){
+//       return "Hi!";
+//     },
+//     sayBye: function(){
+//       return this.firstName + " says bye!";
+//     }
+//   }
+
+/* Write an ES2015 Version */
+const instructor2 = {
+    firstName: "Colt",
+    sayHi(){
+        return "Hi!";
+    },
+    sayBye(){
+        return this.firstName + " says bye!";
+    }
+}
+
+// Write a function which generates an animal object. The function should accepts 3 arguments:
+
+// species: the species of animal (‘cat’, ‘dog’)
+// verb: a string used to name a function (‘bark’, ‘bleet’)
+// noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+
+function createAnimal(animal, verb, noise){
+    return {
+        species:animal,
+        [verb](){
+            return noise;
+        }
+    }
+}
